df73d3d0b1498a7180db19f2f785d41c36c05bd4
Michael Walker <cmichaelswalker@gmail.com>
2018-06-08 23:26:07 -0700

    Initial MVP of react-fullpage.js with foder structure and README.md TODOs

---
+++ webpack.config.dev.js
@@ -0,0 +1,14 @@
+const merge = require('webpack-merge');
+const webpackConfig = require('./webpack.config');
+
+module.exports = merge(webpackConfig, {
+
+  devtool: 'eval',
+
+  output: {
+    pathinfo: true,
+    publicPath: '/',
+    filename: '[name].js',
+  }
+
+});
