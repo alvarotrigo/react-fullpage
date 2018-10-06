71dca3dc8117adbc3eb7956f2867c4ad7881269a
Michael Walker <cmichaelswalker@gmail.com>
2018-10-06 08:07:23 -0700

    Using Fragment component to wrap outermost fullpage sections

---
+++ components/Wrapper/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React, { Fragment } from 'react';
+
+const Wrapper = ({ children }) => <Fragment>{children}</Fragment>;
+
+export default Wrapper;
