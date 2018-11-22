735e25f53898e8ea0bb28189e1c540823ad70551
Alvaro Trigo López <atrigol@gmail.com>
2018-11-22 18:41:54 +0100

    - Updating version & dist files (for npm purposes)

--- dist/react-fullpage-umd.js
+++ dist/react-fullpage-umd.js
@@ -1,5 +1,5 @@
 /*!
- * @fullpage/react-fullpage 0.1.5
+ * @fullpage/react-fullpage 0.1.6
  * https://github.com/alvarotrigo/react-fullpage
  * @license https://github.com/alvarotrigo/react-fullpage#license
  *
