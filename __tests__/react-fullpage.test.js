1a61f0b10dcad1fb3ccfa5cf29bafee8eb293686
Michael Walker <mwalker@pinterest.com>
2019-01-29 22:02:13 -0800

    content assertions

--- __tests__/react-fullpage.test.js
+++ __tests__/react-fullpage.test.js
@@ -2,23 +2,30 @@ import React from 'react';
 import { render, waitForElement } from 'react-testing-library';
 import ReactFullpage from '../components';
 
-describe('<FullPages />', () => {
-  it('FullPages', async () => {
-    const { debug, getByText, getByTestId } = render(
-      <div data-testid="test">
+describe('<ReactFullpage />', () => {
+  const sectionOneContent = 'Section 1';
+  const sectionTwoContent = 'Section 2';
+  const testId = 'test';
+
+  it('renders fullpage sections', async () => {
+    const { getByText, getByTestId } = render(
+      <div data-testid={testId}>
         <ReactFullpage
           navigation
           render={({ fullpageApi }) => {
             return (
               <ReactFullpage.Wrapper>
-                <div className="section">Section 1</div>
-                <div className="section">Section 2</div>
+                <div className="section">{sectionOneContent}</div>
+                <div className="section">{sectionTwoContent}</div>
               </ReactFullpage.Wrapper>
-            )
+            );
           }}
         />
       </div>
     );
-    await waitForElement(() => getByTestId('test'));
+
+    await waitForElement(() => getByTestId(testId));
+    await waitForElement(() => getByText(sectionOneContent));
+    await waitForElement(() => getByText(sectionTwoContent));
   });
 });
