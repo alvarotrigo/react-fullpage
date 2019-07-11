f4c6af297196c8f1b39e8bb1c1d415236468ba90
Alvaro Trigo López <atrigol@gmail.com>
2019-07-11 19:09:02 +0200

    - Examples: adding Gatsby example - Examples: beautifying Gatsby example & syncing with the rest

---
+++ examples/gatsby/gatsby-node.js
@@ -0,0 +1,15 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /@fullpage/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+}
+;
