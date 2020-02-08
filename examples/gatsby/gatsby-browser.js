3f9b7dc32405ff267a1f3841de9310ec840a01c0
Alvaro Trigo López <atrigol@gmail.com>
2020-02-08 21:26:20 +0100

    - Fixed gatsby example: failing when using autoScrolling:false #175

--- examples/gatsby/gatsby-browser.js
+++ examples/gatsby/gatsby-browser.js
@@ -1,1 +1,15 @@
-import "./src/styles/styles.css"
+require('./src/styles/styles.css')
+
+
+exports.shouldUpdateScroll = ({
+    routerProps: { location },
+    getSavedScrollPosition
+  }) => {
+  
+    if (location.hash) {
+      return false;
+    }
+  
+    return true;
+};
+  
