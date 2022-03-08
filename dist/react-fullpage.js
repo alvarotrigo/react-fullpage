20bd772beaf1fae95dc8fa20d386df2849c6aa52
Alvaro Trigo López <atrigol@gmail.com>
2022-03-08 18:55:45 +0100

    - Updating version & dist files

--- dist/react-fullpage.js
+++ dist/react-fullpage.js
@@ -1,5 +1,5 @@
 /*!
- * @fullpage/react-fullpage 0.1.21
+ * @fullpage/react-fullpage 0.1.22
  * https://github.com/alvarotrigo/react-fullpage
  * @license https://github.com/alvarotrigo/react-fullpage#license
  *
