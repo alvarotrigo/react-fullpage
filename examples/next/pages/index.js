e651b668d6674311640d49f208bc1ec7c391c625
Alvaro Trigo López <atrigol@gmail.com>
2022-11-03 20:36:28 +0100

    - Updating to fullPage.js version 4.0.12

--- examples/next/pages/index.js
+++ examples/next/pages/index.js
@@ -5,9 +5,7 @@ import Head from 'next/head'
 
 // NOTE: if using fullpage extensions/plugins put them here and pass it as props.
 const pluginWrapper = () => {
-  /*
-  * require('../static/fullpage.scrollHorizontally.min.js'); // Optional. Required when using the "scrollHorizontally" extension.
-  */
+  require('../static/fullpage.scrollHorizontally.min.js'); // Optional. Required when using the "scrollHorizontally" extension.
 };
 
 
